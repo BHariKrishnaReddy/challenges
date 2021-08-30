e1243e0a37b25d5f50509da4fc306132075cda17
B HariKrishna reddy <45511185+BHariKrishnaReddy@users.noreply.github.com>
2021-08-30 22:32:20 +0530

    Update CodeChef Easyproblems.py

--- CodeChef Easyproblems.py
+++ CodeChef Easyproblems.py
@@ -3,7 +3,6 @@ a,b,x,y=map(int,input().split())
 print(a*x+b*y)
 
 
-
 #VALTRI
 n=int(input())
 if n%5==0 or n%6==0:
@@ -20,3 +19,14 @@ elif n%5==0 or n%11==0:
     print("ONE")
 else:
     print("NONE")
+    
+    
+#DIFACTRS
+c,fl,n =0,[],int(input())
+for i in range(1,n+1):
+    if n%i==0:
+        c+=1
+        fl.append(i)
+print(c)
+for i in fl:
+  print(i,end=" ")
